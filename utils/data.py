3ff602889047ca4f0d333f50a0d9affa7de97813
nbetts2020 <114270814+nbetts2020@users.noreply.github.com>
2024-09-13 22:40:41 -0700

    updated truncation functionality from 'right' to 'left'

--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,8 @@
 import torch
 from torch.utils.data import Dataset
+from utils.config import *
 
-class ArticlePriceDataset(Dataset):
+class ArticlePriceDataset(torch.utils.data.Dataset):
     def __init__(self, articles, prices, tokenizer):
         self.articles = articles
         self.prices = prices
@@ -12,18 +13,17 @@ class ArticlePriceDataset(Dataset):
 
     def __getitem__(self, idx):
         article = self.articles[idx]
-        price = self.prices[idx]
-        encoding = self.tokenizer(
-            article,
+        tokens = self.tokenizer.tokenize(article)
+        if len(tokens) > block_size:
+            tokens = tokens[-block_size:]  # Keep the last block_size tokens
+        encoding = self.tokenizer.encode_plus(
+            self.tokenizer.convert_tokens_to_string(tokens),
             truncation=True,
             padding='max_length',
-            max_length=256,
+            max_length=block_size,
             return_tensors='pt'
         )
-        input_ids = encoding['input_ids'].squeeze(0)  # Remove batch dimension
-        attention_mask = encoding['attention_mask'].squeeze(0)
-        return {
-            'input_ids': input_ids,
-            'attention_mask': attention_mask,
-            'labels': torch.tensor(float(price), dtype=torch.float)
-        }
+        input_ids = encoding['input_ids'].squeeze()
+        attention_mask = encoding['attention_mask'].squeeze()
+        label = torch.tensor(self.prices[idx], dtype=torch.float)
+        return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': label}
