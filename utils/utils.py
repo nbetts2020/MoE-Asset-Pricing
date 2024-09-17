d42e28a88db5b8e560d8f97b641a1186105f25de
nbetts2020 <114270814+nbetts2020@users.noreply.github.com>
2024-09-17 13:30:38 -0700

    changed dataset loading

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,20 @@
 import torch.nn as nn
 from torch.nn import init
 
+import os
+from huggingface_hub import login
+from datasets import load_dataset
+from dotenv import load_dotenv
+
 def kaiming_init_weights(m):
     if isinstance(m, nn.Linear):
         init.kaiming_normal_(m.weight)
+
+def get_data():
+    load_dotenv('/content/MoE-Asset-Pricing/.env')
+    hf_token = os.getenv('HF_TOKEN')
+
+    login(hf_token)
+    dataset = load_dataset("nbettencourt/SC454k-valid")
+    df = dataset['test'].to_pandas().drop(columns=['Unnamed: 0'])
+    return df
