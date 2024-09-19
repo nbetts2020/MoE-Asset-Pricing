3afb161cf6a7fe004acb3ab46c2833f4b62b8eac
nbetts2020 <114270814+nbetts2020@users.noreply.github.com>
2024-09-19 12:53:41 -0700

    config updated with llrd params

--- utils/config.py
+++ utils/config.py
@@ -4,12 +4,14 @@ import torch
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 n_embed = 192        # Embedding dimension
-n_head = 12        # Number of attention heads
+n_head = 12          # Number of attention heads
 n_layer = 96         # Number of transformer blocks
-block_size = 16384     # Maximum sequence length
+block_size = 16384   # Maximum sequence length
 dropout = 0.1        # Dropout rate
 num_experts = 8      # Number of experts in the MoE layer
 top_k = 2            # Number of experts to use per token
 learning_rate = 2e-5
 EPOCHS = 20
 BATCH_SIZE = 16
+
+LR_DECAY = 0.95      # Decay rate per layer
