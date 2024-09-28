0852e8f1269c7cc4f3dea725c13596a710c5c490
Nick Bettencourt <nbettencourt2020@gmail.com>
2024-09-28 13:49:08 -0700

    updated to reflect mrb changes

--- utils/data.py
+++ utils/data.py
@@ -3,27 +3,24 @@ from torch.utils.data import Dataset
 from utils.config import *
 
 class ArticlePriceDataset(torch.utils.data.Dataset):
-    def __init__(self, articles, prices, tokenizer):
+    def __init__(self, articles, prices, sectors, tokenizer):
         self.articles = articles
         self.prices = prices
+        self.sectors = sectors
         self.tokenizer = tokenizer
 
     def __len__(self):
         return len(self.articles)
 
     def __getitem__(self, idx):
-        article = self.articles[idx]
-        tokens = self.tokenizer.tokenize(article)
-        if len(tokens) > block_size:
-            tokens = tokens[-block_size:]  # Keep the last block_size tokens
-        encoding = self.tokenizer.encode_plus(
-            self.tokenizer.convert_tokens_to_string(tokens),
+        encoding = self.tokenizer(
+            self.articles[idx],
             truncation=True,
             padding='max_length',
             max_length=block_size,
             return_tensors='pt'
         )
-        input_ids = encoding['input_ids'].squeeze()
-        attention_mask = encoding['attention_mask'].squeeze()
-        label = torch.tensor(self.prices[idx], dtype=torch.float)
-        return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': label}
+        item = {key: val.squeeze(0) for key, val in encoding.items()}
+        item['labels'] = torch.tensor(self.prices[idx], dtype=torch.float)
+        item['sector'] = self.sectors[idx]  # Include sector information
+        return item
