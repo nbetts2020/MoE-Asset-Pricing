d0545cbc3ac1d6353735c1966df9d62d9b39b7b2
Nick Bettencourt <nbettencourt2020@gmail.com>
2024-10-03 12:44:40 -0700

    updated to include sector info

--- utils/data.py
+++ utils/data.py
@@ -1,8 +1,6 @@
-import torch
-from torch.utils.data import Dataset
-from utils.config import *
+# data.py
 
-class ArticlePriceDataset(torch.utils.data.Dataset):
+class ArticlePriceDataset(Dataset):
     def __init__(self, articles, prices, sectors, tokenizer):
         self.articles = articles
         self.prices = prices
@@ -13,14 +11,19 @@ class ArticlePriceDataset(torch.utils.data.Dataset):
         return len(self.articles)
 
     def __getitem__(self, idx):
+        article = self.articles[idx]
+        price = self.prices[idx]
+        sector = self.sectors[idx]
         encoding = self.tokenizer(
-            self.articles[idx],
+            article,
             truncation=True,
             padding='max_length',
             max_length=block_size,
             return_tensors='pt'
         )
-        item = {key: val.squeeze(0) for key, val in encoding.items()}
-        item['labels'] = torch.tensor(self.prices[idx], dtype=torch.float)
-        item['sector'] = self.sectors[idx]  # Include sector information
-        return item
+        input_ids = encoding['input_ids'].squeeze()
+        return {
+            'input_ids': input_ids,
+            'labels': torch.tensor(price, dtype=torch.float),
+            'sector': sector
+        }
