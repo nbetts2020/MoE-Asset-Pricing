dcb1ed5883b9e6ddb5ba6ce21d521058c61e9546
Nick Bettencourt <nbettencourt2020@gmail.com>
2024-10-27 20:07:11 -0700

    updated imports

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Install system dependencies
+set -e
+
 apt-get update
-apt-get install -y libcairo2-dev
-# Now install Python dependencies
+apt-get install -y libcairo2-dev build-essential python3-dev
+
+pip install --upgrade pip setuptools wheel
+
+pip install peewee psycopg2 rmm-cu12
+
 pip install -r requirements.txt
+
+pip install flash_attn
