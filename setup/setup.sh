a7917c53c5f7f20f042cc0ff25d872b4274de31b
Nick Bettencourt <nbettencourt2020@gmail.com>
2024-10-27 20:44:23 -0700

    pip issues

--- setup/setup.sh
+++ setup/setup.sh
@@ -14,20 +14,26 @@ sudo apt-get install -y \
 
 if ! command -v pip3 &> /dev/null
 then
-    echo "pip3 could not be found, installing pip..."
+    echo "pip3 could not be found, installing pip3..."
     sudo apt-get install -y python3-pip
 fi
 
-pip3 install --upgrade pip setuptools wheel
+sudo -H pip3 install --upgrade pip setuptools wheel
 
-pip3 install peewee psycopg2-binary rmm-cu12
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install --upgrade pip setuptools wheel
+
+pip install peewee psycopg2-binary rmm-cu12
 
 if [ -f requirements.txt ]; then
-    pip3 install -r requirements.txt
+    pip install -r requirements.txt
 else
     echo "requirements.txt not found. Skipping this step."
 fi
 
-pip3 install flash_attn
+pip install flash_attn
 
 echo "Setup and upload completed successfully."
