566a72adc99bdd025d9a29ff4e3d1e34b9c372da
Nick Bettencourt <nbettencourt2020@gmail.com>
2025-01-27 23:00:19 -0800

    numeric_only arg

--- main.py
+++ main.py
@@ -173,6 +173,9 @@ def main():
         config.N_HEAD = 4
         config.N_LAYER = 12
         config.BLOCK_SIZE = 1024
+    if args.numeric_only:
+        logging.info("Switching to numeric_only hyperparameters for faster execution.")
+        config.BLOCK_SIZE = 64
 
     # Additional overrides
     if args.n_embed is not None:
