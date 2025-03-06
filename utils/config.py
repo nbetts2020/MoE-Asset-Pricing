d5103f5047130b1bff407691e8bbeea02a1c289c
Nick Bettencourt <nbettencourt2020@gmail.com>
2025-03-06 00:26:27 -0800

    updated configs

--- utils/config.py
+++ utils/config.py
@@ -7,13 +7,13 @@ class Config:
     N_EMBED = 384        # Embedding dimension
     N_HEAD = 24          # Number of attention heads
     N_LAYER = 96         # Number of transformer blocks
-    BLOCK_SIZE = 65536   # Maximum sequence length
+    BLOCK_SIZE = 4096    # Maximum sequence length during rl attention - not max context window
     DROPOUT = 0.1        # Dropout rate
     NUM_EXPERTS = 8      # Number of experts in the MoE layer
     TOP_K = 2            # Number of experts to use per token
     LEARNING_RATE = 2e-5
     EPOCHS = 20 
-    BATCH_SIZE = 16
+    BATCH_SIZE = 8       # Effective batch size is 32 with 4 grad accumulation steps
     
     LR_DECAY = 0.95      # Decay rate per layer
     LAMBDA_SI = 0.1      # Lambda val for synaptic intelligence
