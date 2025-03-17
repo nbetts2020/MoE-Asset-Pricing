73f34a4e51943e2d910a52b51c4574439ff125d2
Nick Bettencourt <nbettencourt2020@gmail.com>
2025-03-17 14:07:48 -0700

    removing llrd

--- utils/config.py
+++ utils/config.py
@@ -7,7 +7,7 @@ class Config:
     N_EMBED = 384        # Embedding dimension
     N_HEAD = 24          # Number of attention heads
     N_LAYER = 96         # Number of transformer blocks
-    BLOCK_SIZE = 4096    # Maximum sequence length during rl attention - not max context window
+    BLOCK_SIZE = 2048    # Maximum sequence length during rl attention - not max context window
     DROPOUT = 0.1        # Dropout rate
     NUM_EXPERTS = 4      # Number of experts in the MoE layer
     TOP_K = 2            # Number of experts to use per token
@@ -15,7 +15,7 @@ class Config:
     EPOCHS = 3 
     BATCH_SIZE = 16       # Effective batch size is 32 with 2 grad accumulation steps
     
-    LR_DECAY = 0.95      # Decay rate per layer
+    LR_DECAY = 1      # Decay rate per layer
     LAMBDA_SI = 0.1      # Lambda val for synaptic intelligence
 
 config = Config()
