28fffb921add1aef45b47ab4d4ef183bdeda4cd7
Nick Bettencourt <nbettencourt2020@gmail.com>
2025-03-30 12:41:09 -0700

    updated params

--- utils/config.py
+++ utils/config.py
@@ -4,15 +4,16 @@ import torch
 
 class Config:
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    N_EMBED = 384        # Embedding dimension
-    N_HEAD = 24          # Number of attention heads
-    N_LAYER = 96         # Number of transformer blocks
-    BLOCK_SIZE = 2048    # Maximum sequence length during rl attention - not max context window
+    N_EMBED = 1792        # Embedding dimension
+    N_HEAD = 32          # Number of attention heads
+    N_LAYER = 24         # Number of transformer blocks
+    BLOCK_SIZE = 4048    # Maximum sequence length during rl attention - not max context window
+    CONTEXT_WINDOW = 16384
     DROPOUT = 0.1        # Dropout rate
     NUM_EXPERTS = 4      # Number of experts in the MoE layer
     TOP_K = 2            # Number of experts to use per token
     LEARNING_RATE = 3e-4
-    EPOCHS = 3 
+    EPOCHS = 1 
     BATCH_SIZE = 16       # Effective batch size is 32 with 2 grad accumulation steps
     LAMBDA_EBM = 0.1
     
