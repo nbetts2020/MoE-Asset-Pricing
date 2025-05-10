04a46064afb226e979e6512d9b1efdba3e5ce66a
Nick Bettencourt <nbettencourt2020@gmail.com>
2025-05-09 19:43:28 -0700

    ulysses and ring degrees

--- utils/config.py
+++ utils/config.py
@@ -9,14 +9,15 @@ class Config:
     N_LAYER = 24         # Number of transformer blocks
     BLOCK_SIZE = 4096    # Maximum sequence length during rl attention - not max context window
     CONTEXT_WINDOW = 16384
-    DROPOUT = 0.1        # Dropout rate
+    DROPOUT = 0.0        # Dropout rate
     NUM_EXPERTS = 4      # Number of experts in the MoE layer
     TOP_K = 2            # Number of experts to use per token
     LEARNING_RATE = 3e-4
-    EPOCHS = 1 
+    EPOCHS = 1
     BATCH_SIZE = 16       # Effective batch size is 32 with 2 grad accumulation steps
     LAMBDA_EBM = 0.1
-    
+    SP_ULYSSES_DEGREE = 1
+    SP_RING_DEGREE    = 2
     LR_DECAY = 1      # Decay rate per layer
     LAMBDA_SI = 0.1      # Lambda val for synaptic intelligence
 
